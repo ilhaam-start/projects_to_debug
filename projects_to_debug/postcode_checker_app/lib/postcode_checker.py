73679b1de4e24833cd587cdd0d696bf2a16569c0
Ilhaam <ilhaam777@gmail.com>
2023-05-23 16:43:13 +0100

    "changes updated"

--- projects_to_debug/postcode_checker_app/lib/postcode_checker.py
+++ projects_to_debug/postcode_checker_app/lib/postcode_checker.py
@@ -5,6 +5,6 @@ class PostcodeChecker():
         if postcode is None:
             return False
         return re.match(
-            r"^[A-Za-z]{1,2}[0-9][A-Za-z]? [0-9][A-Za-z]{2}$",
+            r"^[A-Za-z]{1,2}[0-9]{1,2}[A-Za-z]? [0-9][A-Za-z]{2}$", #This pattern ensures that the postcode starts with 1 or 2 letters, then 1 or 2 numbers, an optional letter, a space and then two letters.
             postcode,
             re.IGNORECASE) is not None
